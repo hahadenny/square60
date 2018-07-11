075f535b71b438cba470e0474ff99ab38709be72
Denny Choi <denny0709@hotmail.com>
2018-07-11 01:07:12 -0400

    TESTb

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('content')
-	<div class="wrapper wrapper-bg" style="min-height:835px;">aabbccddeeff
+	<div class="wrapper wrapper-bg" style="min-height:835px;">ab
 		<div class="content" style="flex:none;">
 			<div class="header test-border header-bg" style="border-bottom: solid 1px #282828;">
 				<div class="promo" style="padding-bottom:0px;">
